74897cf6d2987720760ae0f12962583dbab7cf86
Rishabh Karanth <rkaranth@terpmail.umd.edu>
2023-03-30 23:46:05 -0400

    fix team5 ghost-attack

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -76,8 +76,7 @@ public class Ghost {
 
   public boolean attack() {
     if (is_pacman_in_range() == true) {
-      myMap.attack(myName);
-      return false;
+      return myMap.attack(myName);
     } 
     else {
       return false;
